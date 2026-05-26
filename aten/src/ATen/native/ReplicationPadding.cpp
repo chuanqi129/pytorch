d536fe7534539449f246003882afdab843950053
Stonepia <tong.su@intel.com>
2026-05-26 03:33:18 -0700

    Fix for intel-sandbox/agentic_xpu#2 (attempt 3)
    [cuda_xpu_alignment] Fix `replication_pad{2d,3d}` meta accepting negative output dims

--- aten/src/ATen/native/ReplicationPadding.cpp
+++ aten/src/ATen/native/ReplicationPadding.cpp
@@ -111,7 +111,7 @@ TORCH_META_FUNC(replication_pad2d) (
   int64_t oheight = iheight + pad_t + pad_b;
   int64_t owidth  = iwidth + pad_l + pad_r;
 
-  TORCH_CHECK(owidth >= 1 || oheight >= 1,
+  TORCH_CHECK(owidth >= 1 && oheight >= 1,
       "input (H: ", iheight, ", W: ", iwidth, " ) is too small."
       " Calculated output H: ", oheight, " W: ", owidth);
 
@@ -157,7 +157,7 @@ TORCH_META_FUNC(replication_pad3d) (
   int64_t oheight = iheight + ptop + pbottom;
   int64_t owidth  = iwidth + pleft + pright;
 
-  TORCH_CHECK(owidth >= 1 || oheight >= 1 || odepth >= 1,
+  TORCH_CHECK(owidth >= 1 && oheight >= 1 && odepth >= 1,
       "input (D: ", idepth, " H: ", iheight, ", W: ", iwidth,
       ") is too small."
       " Calculated output D: ", odepth, " H: ", oheight, " W: ", owidth);
